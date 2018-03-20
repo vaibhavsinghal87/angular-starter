0ccba0c12bb9dabbedd51bf6c42810857b3ba837
Vaibhav Singhal <vaibhavsinghal87@gmail.com>
2018-03-20 20:47:12 +0530

    2. Application Shell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
