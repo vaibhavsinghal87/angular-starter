08e9e7f76382a87c6acdb38ac08ec638f4bb44d8
vaibhavsinghal87 <vaibhavsinghal87@gmail.com>
2018-03-20 21:54:39 +0530

    4. Displaying a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -9,14 +11,15 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero:Hero = {
-    id: 1,
-    name: 'Vaibhav Singhal'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero){
+    this.selectedHero = hero;
+  }
 }
