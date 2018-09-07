e5d911d33f11d67ebcbe9d20298578fc03bc23ac
vaibhavsinghal87 <vaibhavsinghal87@gmail.com>
2018-09-07 23:19:45 +0530

    Angular somehow adds double slashes in path. Removing double slashes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 
